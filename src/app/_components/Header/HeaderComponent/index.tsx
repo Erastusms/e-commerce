569edb20046c159c8bdced6dc36490365351b7e1
Erastus Mauliate <erastus.mauliate@it.component.astra.co.id>
2024-03-27 15:02:30 +0700

    fix(button): use button in HeaderComponent

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
@@ -8,10 +10,18 @@ import { Gutter } from '../../Gutter'
 import classes from './index.module.scss'
 import { HeaderNav } from '../Nav'
 import MobileNav from '../MobileNav'
+import { noHeaderFooterUrls } from '../../../constants'
+import { usePathname } from 'next/navigation'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
+
   return (
-    <nav className={[classes.header]}>
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
       <Gutter className={classes.wrap}>
         <Link href="/">
           <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
